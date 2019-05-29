94b837130776d35140e409ca411a0d19cabd4905
studioshane <studioshane@gmail.com>
2019-05-29 21:15:53 +0100

    mapbox api call added

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const request = require('request');
+const api = require('./API');
 
-fetch(URL)
-	.then(resp => resp.json())
-	.then(data => console.log(data));
+request({ url: api.darkSky + api.c, json: true }, (error, response) => {
+	const data = response.body;
+	console.log(
+		data.daily.data[0].summary +
+			' It is currently ' +
+			data.currently.temperature +
+			' degrees out. The current chance of percipitation is ' +
+			data.currently.precipProbability +
+			'%'
+	);
+});
+
+request({ url: api.mapBox, json: true }, (error, response) => {
+	const coords = response.body.features[0].center;
+	console.log('Latitude: ' + coords[1] + ' Longitude: ' + coords[0]);
+});
